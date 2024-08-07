8064d8bea08737e089d049617ddead4af242cf4c
Paulo Sérgio dos Santos Júnior <paulossjunior@gmail.com>
2024-08-07 18:09:27 -0300

    testando o looging e criando um job de um minuto

--- job_daily_report.py
+++ job_daily_report.py
@@ -5,7 +5,17 @@ import nltk
 import re
 from dotenv import load_dotenv
 import os
+import logging
 
+# Configurar logging
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s [%(levelname)s] %(message)s',
+    handlers=[
+        logging.FileHandler("tracking.log"),
+        logging.StreamHandler()
+    ]
+)
 
 # Carregar variáveis de ambiente do arquivo .env
 load_dotenv()
@@ -21,29 +31,28 @@ webhook_url = os.getenv('WEBHOOK_URL')
 
 # Autenticar no Jira
 jira = JIRA(basic_auth=(jira_username, jira_api_token), options={'server': jira_url})
+logging.info("Autenticado no Jira.")
 
 def summarize_text(text, max_chars=500):
     """Resume o texto para que não exceda o limite de caracteres."""
-    # Dividir o texto em frases
+    logging.debug(f"Resumindo texto com limite de {max_chars} caracteres.")
     sentences = nltk.sent_tokenize(text, language='portuguese')
-
-    # Filtrar frases
     filtered_sentences = [sentence for sentence in sentences if sentence.strip()]
-
-    # Resumir o texto
-    summary = ' '.join(filtered_sentences[:5])  # Ajustar o número de frases conforme necessário
+    summary = ' '.join(filtered_sentences[:5]) 
 
     if len(summary) > max_chars:
-        return summary[:max_chars]  # Corta o texto para o limite de caracteres
+        logging.debug("Resumo de texto cortado para caber no limite de caracteres.")
+        return summary[:max_chars] 
     return summary
 
 def split_message(message, max_chars=2000):
     """Divide a mensagem em partes menores que respeitam o limite de caracteres."""
+    logging.debug(f"Dividindo mensagem em partes de até {max_chars} caracteres.")
     return [message[i:i+max_chars] for i in range(0, len(message), max_chars)]
 
 def clean_comment(comment):
     """Remove o nome da pessoa dos comentários."""
-    # Remover padrões como [Nome|<texto>]
+    logging.debug("Limpando comentário para remover nomes.")
     clean_comment = re.sub(r'\[.*?\|', '', comment)
     clean_comment = re.sub(r'\]', '', clean_comment)
     return clean_comment.strip()
@@ -51,8 +60,11 @@ def clean_comment(comment):
 def format_date(date_str):
     """Formata a data no formato dd/mm/yyyy, retorna 'Data não disponível' se a data for inválida."""
     try:
-        return datetime.strptime(date_str, '%Y-%m-%d').strftime('%d/%m/%Y')
+        formatted_date = datetime.strptime(date_str, '%Y-%m-%d').strftime('%d/%m/%Y')
+        logging.debug(f"Data formatada: {formatted_date}")
+        return formatted_date
     except ValueError:
+        logging.warning(f"Data inválida encontrada: {date_str}")
         return 'Data não disponível'
 
 def is_overdue(due_date):
@@ -60,16 +72,18 @@ def is_overdue(due_date):
     try:
         today = datetime.now().date()
         due_date_obj = datetime.strptime(due_date, '%Y-%m-%d').date()
-        return today > due_date_obj
+        overdue = today > due_date_obj
+        logging.debug(f"Verificando se a tarefa está em atraso: {overdue}")
+        return overdue
     except ValueError:
+        logging.warning(f"Data de vencimento inválida encontrada: {due_date}")
         return False
 
 def process_board(board_id):
     try:
-        # Obter os sprints do board específico
+        logging.info(f"Processando board {board_id}")
         sprints = jira.sprints(board_id)
 
-        # Encontrar o sprint ativo
         sprint_id = None
         for sprint in sprints:
             if sprint.state == 'active':
@@ -77,28 +91,24 @@ def process_board(board_id):
                 sprint_name = sprint.name
                 sprint_start_date = datetime.strptime(sprint.startDate, '%Y-%m-%dT%H:%M:%S.%f%z').strftime('%d/%m/%Y')
                 sprint_end_date = datetime.strptime(sprint.endDate, '%Y-%m-%dT%H:%M:%S.%f%z').strftime('%d/%m/%Y')
+                logging.info(f"Sprint ativo encontrado: {sprint_name}")
                 break
 
         if sprint_id:
-            # Data de hoje
-            #today = datetime.now().strftime('%Y-%m-%d')
-
-            # Buscar todas as tarefas do sprint ativo
             jql_query = (f'sprint = {sprint_id} AND (status = "In Progress" OR (status = "Done"'
                          f'AND  updated >= startOfDay()))')
             issues = jira.search_issues(jql_query)
-            print (issues)
-            # Organizar tarefas por pessoa
+            logging.debug(f"{len(issues)} tarefas encontradas no sprint ativo.")
+
             tasks_by_person = {}
             for issue in issues:
                 assignee = issue.fields.assignee.displayName if issue.fields.assignee else 'Não Atribuído'
                 issue_key = issue.key
                 issue_summary = issue.fields.summary
                 status = issue.fields.status.name
-                start_date = issue.fields.created[:10]  # Assumindo que a data de início é a data de criação
+                start_date = issue.fields.created[:10] 
                 due_date = issue.fields.duedate if issue.fields.duedate else 'Sem data de conclusão'
 
-                # Adicionar informações da tarefa
                 task_details = {
                     'key': issue_key,
                     'summary': issue_summary,
@@ -110,7 +120,8 @@ def process_board(board_id):
                     'impediments': []
                 }
 
-                # Buscar comentários da tarefa
+                logging.debug(f"Processando tarefa {issue_key} para {assignee}.")
+
                 comments = jira.comments(issue)
                 for comment in comments:
                     comment_body = clean_comment(comment.body)
@@ -122,15 +133,13 @@ def process_board(board_id):
                 if assignee not in tasks_by_person:
                     tasks_by_person[assignee] = {'completed': [], 'in_progress': [], 'next_tasks': []}
 
-                # Adicionar tarefa aos detalhes da pessoa
-                if status.lower() in ['done', 'concluído']:  # Ajuste conforme necessário
+                if status.lower() in ['done', 'concluído']:  
                     tasks_by_person[assignee]['completed'].append(task_details)
-                elif status.lower() in ['in progress', 'em andamento']:  # Ajuste conforme necessário
+                elif status.lower() in ['in progress', 'em andamento']:  
                     tasks_by_person[assignee]['in_progress'].append(task_details)
                 else:
                     tasks_by_person[assignee]['next_tasks'].append(task_details)
 
-            # Enviar a mensagem de cabeçalho
             header_content = (
                 f"# Relatório Diário: {sprint_name} ({sprint_start_date} - {sprint_end_date})\n"
             )
@@ -138,12 +147,9 @@ def process_board(board_id):
             for msg in header_messages:
                 data = {'content': msg}
                 response = requests.post(webhook_url, json=data)
-                print(f"Status Code do Cabeçalho: {response.status_code}")
-                print(f"Resposta do Discord do Cabeçalho: {response.text}")
+                logging.info(f"Mensagem de cabeçalho enviada. Status Code: {response.status_code}")
 
-            # Enviar uma mensagem separada para cada pessoa
             for person, task_info in tasks_by_person.items():
-                # Construir o conteúdo da mensagem
                 base_content = (
                     f"# Nome: {person}\n\n"
                     f"## Tarefas Em Andamento:\n"
@@ -189,30 +195,29 @@ def process_board(board_id):
                 else:
                     base_content += "  - Nenhuma próxima tarefa identificada.\n"
 
-                # Dividir a mensagem se necessário
                 messages = split_message(base_content)
 
-                # Enviar cada parte da mensagem para o Discord
                 for msg in messages:
                     data = {'content': msg}
                     response = requests.post(webhook_url, json=data)
-                    print(f"Status Code da Pessoa: {response.status_code} para {person}")
-                    print(f"Resposta do Discord da Pessoa: {response.text} para {person}")
+                    logging.info(f"Mensagem enviada para {person}. Status Code: {response.status_code}")
 
-                # Adicionar uma linha em branco entre os relatórios
-                print("\n" + "-"*50 + "\n")
+                logging.debug(f"Relatório de {person} processado.\n" + "-"*50)
 
     except Exception as e:
-        print(f"Ocorreu um erro ao processar o board {board_id}: {e}")
+        logging.error(f"Ocorreu um erro ao processar o board {board_id}: {e}")
 
 def main():
     try:
-        # Obter todos os boards
+        logging.info("Iniciando processo principal.")
         boards = jira.boards()
+        logging.info(f"{len(boards)} boards encontrados.")
 
-        # Processar cada board
         for board in boards:
             process_board(board.id)
 
     except Exception as e:
-        print(f"Ocorreu um erro: {e}")
+        logging.error(f"Ocorreu um erro ao buscar boards: {e}")
+
+if __name__ == "__main__":
+    main()
