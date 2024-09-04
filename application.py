77e14840b0f69dba4ec8a08ec23c5a2157788574
hei-lima <heitor.lipeixoto@gmail.com>
2024-09-04 14:36:03 -0300

    Added env vars for changing schedule time

--- application.py
+++ application.py
@@ -4,6 +4,7 @@ from job_resume_sprint import main as main_job_resume_sprint
 from job_resume_project import main as main_job_resume_project
 import schedule
 import time
+import os
 
 def main():
     main_job_daily_report()
@@ -12,13 +13,13 @@ def main():
     main_job_resume_project()
 
 if __name__ == "__main__":
+    schedule_time = os.getenv("SCHEDULE_TIME", "17:00")
     
-  
-    schedule.every().monday.at("17:00").do(main)
-    schedule.every().tuesday.at("17:00").do(main)
-    schedule.every().wednesday.at("17:00").do(main)
-    schedule.every().thursday.at("17:00").do(main)
-    schedule.every().friday.at("17:00").do(main)
+    schedule.every().monday.at(schedule_time).do(main)
+    schedule.every().tuesday.at(schedule_time).do(main)
+    schedule.every().wednesday.at(schedule_time).do(main)
+    schedule.every().thursday.at(schedule_time).do(main)
+    schedule.every().friday.at(schedule_time).do(main)
 
     while True:
         schedule.run_pending()
